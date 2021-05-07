4ff78e75f9dae32af032ae311f5b3b2b645be7fa
QuocHuy <huyhvker@gmail.com>
2021-05-07 12:24:52 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,27 @@
+package basic.dev;
+/**
+ * lap 03
+ * @author TranQuocHuy
+ *
+ */
+public class MainApp {
+
+	public static void main(String[] args) {
+		String firstName = "Tran Quoc";
+		String lastName = "Huy";
+		int old = 20;
+		float markMath = 9f;
+		boolean passed = true;
+		
+		System.out.println("Ho va ten: ");
+		System.out.println(firstName + " " + lastName );
+		System.out.println("Tuoi: ");
+		System.out.println(old);
+		System.out.println("Diem toan: " + markMath);
+		System.out.println("Passed: " + passed);
+		final float PI = 3.14f;
+		
+
+	}
+
+}
